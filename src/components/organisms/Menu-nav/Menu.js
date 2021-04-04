86e9a633b6f5a870bfb4f31592e0687fa3fc9705
MarW5 <marta.wiese@gmail.com>
2021-04-04 16:23:42 +0200

    Base files

---
+++ src/components/organisms/Menu-nav/Menu.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const MenuNav = ()=>(
+
+    <div>
+            <ul>
+                <li>Kryptowaluty</li>
+                <li>Waluty</li>
+                <li>Surowce</li>
+                <li>WIG 20</li>
+            </ul>
+        </div>
+    
+
+)
+
+
+
+
+export default MenuNav;
+ 
---
+++ src/components/organisms/Snipped.js
@@ -0,0 +1,31 @@
+//hook useState
+// import React, { useState, useEffext, useEffect } from "react"
+// const KlassName =()=>{
+//     //dstrukturyzacja useState
+//     const [state, setState] = useState({
+//         name: "",
+//         average: "",
+//         id: ""
+
+//     })
+
+//     hendleInputChanges= (e) =>{
+//         setState({
+//             ...state,
+//             [e.target.name]: e.target.value,
+
+//         })
+//     }
+
+//     useEffect( ()=>{
+
+//     }, []
+//     //miejsce componentDidUpdate- rzeczy które chcemy robić po edycji stanu komponentu
+//     );
+
+//     return(<div></div>)
+
+// }
+
+// export default KlassName;
+
